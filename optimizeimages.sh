c0975a14bc9864fd8f63f4ea465f964fee9c7f3c
Michael J. Cohen <michael.joseph.cohen@gmail.com>
2019-10-21 15:41:36 -0600

    Uppercase is supposed to be for environment variables only

--- optimizeimages.sh
+++ optimizeimages.sh
@@ -2,27 +2,27 @@
 set -o errexit
 
 # @TODO detect fd and fallback to find
-PNGS=$(fd -e png .)
-JPGS=$(fd -e jpg .)
-#PNGS=$(find . -iname "*.png")
-#JPGS=$(find . -iname "*.jpg")
+pngs=$(fd -e png .)
+jpgs=$(fd -e jpg .)
+#pngs=$(find . -iname "*.png")
+#jpgs=$(find . -iname "*.jpg")
 TMP1="_TMP1.PNG"
 TMP2="_TMP2.PNG"
 
 echo "Optimizing PNG"
-for PNG in ${PNGS}
+for png in ${pngs}
 do
-	BEFORE=$(stat -c %s "${PNG}")
-	printf "	%s: %s " "${PNG}" "${BEFORE}"
-	cp "${PNG}" "${TMP1}"
-	COLORS=$(pngtopnm "${PNG}" | ppmhist -noheader | wc -l)
+	BEFORE=$(stat -c %s "${png}")
+	printf "	%s: %s " "${png}" "${BEFORE}"
+	cp "${png}" "${TMP1}"
+	COLORS=$(pngtopnm "${png}" | ppmhist -noheader | wc -l)
 
 	if [ "$COLORS" -lt 2 ]; then
 		COLORS=2
 	fi
 
 	if [ "$COLORS" -lt 257 ]; then
-	    pngquant ${COLORS} < "${PNG}" > "${TMP1}"
+	    pngquant ${COLORS} < "${png}" > "${TMP1}"
 	fi
 
 	pngcrush -q -l 9 -brute -rem alla "${TMP1}" "${TMP2}"
@@ -31,7 +31,7 @@ do
 
 	AFTER=$(stat -c %s "${TMP1}")
 	if [ "$AFTER" -lt "$BEFORE" ]; then
-		mv "${TMP1}" "${PNG}"
+		mv "${TMP1}" "${png}"
 		echo "--> ${AFTER}"
 	else
 		echo "(Already optimal)"
@@ -41,15 +41,15 @@ do
 done
 
 echo "Optimizing JPG"
-for JPG in ${JPGS}
+for jpg in ${jpgs}
 do
-	BEFORE=$(stat -c %s "${JPG}")
-	printf "	%s: %s " "${JPG}" "${BEFORE}"
-	jpegtran -optimize -copy none "${JPG}" > "${TMP1}"
+	BEFORE=$(stat -c %s "${jpg}")
+	printf "	%s: %s " "${jpg}" "${BEFORE}"
+	jpegtran -optimize -copy none "${jpg}" > "${TMP1}"
 	AFTER=$(stat -c %s "${TMP1}")
 
 	if [ "$AFTER" -lt "$BEFORE" ]; then
-		mv "${TMP1}" "${JPG}"
+		mv "${TMP1}" "${jpg}"
 		echo "--> ${AFTER}"
 	else
 		echo "(Already optimal)"
